a52ca3e53001eca2710aea9eb12e1358c703fe71
英雄造时势 <zhangpingymd@gmail.com>
2018-10-18 17:12:40 +0800

    完善我的页面

--- src/views/mine/Index.vue
+++ src/views/mine/Index.vue
@@ -9,10 +9,49 @@
           <span>关注 5</span>
           <span>粉丝 5</span>
         </p>
+        <p class="tag">
+          <span><i class="iconfont nan"></i> 95后</span>
+          <span><i class="iconfont level"></i> 8</span>
+          <span>成都</span>
+          <span>水瓶座</span>
+        </p>
       </div>
+    </div>
+    <div class="content">
+      <van-tabs v-model="active">
+        <van-tab title="音乐">
+          <SongList></SongList>
+        </van-tab>
+        <van-tab title="动态">暂未开放，敬请期待</van-tab>
+        <van-tab title="关于我">暂未开放，敬请期待</van-tab>
+      </van-tabs>
+      <van-button plain type="danger" class="exit" @click="logout">退出登录</van-button>
     </div>
   </div>
 </template>
+<script>
+import { Tab, Tabs, Button } from 'vant'
+import SongList from './SongList'
+export default {
+  components: {
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs,
+    [Button.name]: Button,
+    SongList
+  },
+  data () {
+    return {
+      active: 0
+    }
+  },
+  methods: {
+    logout () {
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
 .info{
   width: 100%;height: 47vh;background: url('~@/assets/images/default.png') no-repeat;background-size: 103% 103%;
@@ -30,7 +69,7 @@
     font-size: 0.4rem;font-weight: bold;margin-top: 0.1rem;
   }
   .fan{
-    margin-top: 0.1rem;text-align: center;color: #eee;font-size: 0.25rem;
+    margin-top: 0.13rem;text-align: center;color: #eee;font-size: 0.25rem;
     span{
       display: inline-block;height: 0.25rem;line-height: 0.25rem;
     }
@@ -38,6 +77,36 @@
       padding-right: 0.2rem;margin-right: 0.2rem;border-right: 0.01rem solid #999;
     }
   }
+  .tag{
+    text-align: center;margin-top: 0.25rem;
+    span{
+      padding: 0.04rem 0.13rem;background-color:rgba(204,204,204,0.8);border-radius: 0.3rem;margin: 0 0.03rem;font-size: 0.2rem;color: #fff; 
+    }
+    .iconfont{
+      font-size: 0.2rem;
+    }
+  }
+}
+.content{
+  background-color: #fff;border-radius: 0.12rem;position: relative;top: -0.2rem;overflow: hidden;
+  .van-tabs{
+    border-top-left-radius: 0.12rem;
+    border-top-right-radius: 0.12rem;
+    overflow: hidden;
+    position: static;
+  }
+  .van-tabs__wrap.van-hairline--top-bottom{
+    width: 95%;margin: 0 auto;
+    border-top-left-radius: 0.12rem;
+    border-top-right-radius: 0.12rem;
+    overflow: hidden;
+  }
+  .van-tab__pane{
+    margin-top: 0.2rem;
+  }
+}
+.exit{
+  width: 90%;margin: 0.5rem auto;display: block;border-radius: 0.1rem;
 }
 </style>
 
---
+++ src/views/mine/SongList.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="cls">
+      <h2>歌单(10)</h2>
+      <ul>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+      </ul>
+    </div>
+    <div class="cls">
+      <h2>收藏的歌单</h2>
+      <ul>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+        <li>
+          <img src="~@/assets/images/default.png" alt="">
+          <dl>
+            <dt>歌单名字</dt>
+            <dd>10首，播放20次</dd>
+          </dl>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.cls{
+  h2{
+    height: 0.5rem;padding-left: 0.2rem;font-size: 0.23rem;background-color: #eee;color: #888;line-height: 0.5rem;
+  }
+  ul{
+    width: 100%;padding: 0 2vw;
+  }
+  li{
+    display: flex;justify-content: space-between;height: 14vw;margin: 0.1rem 0;
+    img{
+      width: 14vw;height: 14vw;border-radius: 0.2rem;
+    }
+    dl{
+      width: 78vw;height: 100%;padding: 0;margin: 0;padding-top: 0.1rem;border-bottom: 0.01rem solid #eee;
+      dd{
+        margin: 0;color: #888;font-size: 0.2rem;
+      }
+      dt{
+        font-size: 0.3rem;
+      }
+    }
+  }
+}
+</style>
+
