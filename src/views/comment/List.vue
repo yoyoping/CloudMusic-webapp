3cd4c6c1e62734c212cbd38f38b1fe0da38c34c7
英雄造时势 <zhangpingymd@gmail.com>
2018-11-12 20:50:16 +0800

    完善评论页面开发

--- src/views/comment/List.vue
+++ src/views/comment/List.vue
@@ -1,11 +1,21 @@
 <template>
 	<div class="comment">
-		<router-link to="/">点击</router-link>
+		<div class="link">
+			<img v-lazy="musicDetail.picUrl" alt="">
+			<dl>
+				<dt class="van-ellipsis">耳朵</dt>
+				<dd><a href="javascript:;" class="singer">李荣浩</a></dd>
+			</dl>
+			<i class="iconfont youjiantou"></i>
+		</div>
 	</div>
 </template>
 <script>
-import { mapMutations } from 'vuex'
+import { mapMutations, mapState } from 'vuex'
 export default {
+	computed: {
+		...mapState(['musicDetail'])
+	},
 	created () {
 		this.getList()
 	},
@@ -17,6 +27,7 @@ export default {
 				id: this.$route.params.id
 			}
 			const res = await this.$axios(params)
+			console.log(res)
 			this.SET_TITLE(`评论${res.total}`)
 		}
 	},
@@ -28,5 +39,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.link{
+	height: 26vw;padding: 3vw;display: flex;
+	img{
+		width: 20vw;height: 20vw;border-radius: 0.1rem;justify-content: flex-start;
+	}
+	dl{
+		width: 68vw;padding: 0 6vw 0 4vw;
+	}
+	dt{
+		padding: 0.28rem 0 0.2rem;font-size: 0.3rem;
+	}
+	dd{
+		font-size: 0.16rem;
+	}
+	.singer{
+		color: cadetblue;
+	}
+	.iconfont{
+		font-size: 0.5rem;line-height: 19vw;color: #ddd;
+	}
+}
 </style>
